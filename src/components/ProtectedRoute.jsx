7232c593be7cec614d0acb8e5a3b2cf4cf147515
shanika200721 <shanika.uok2@gmail.com>
2025-04-01 21:07:39 +0530

    remove front end from repo

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = () => {
-  const isAuthenticated = localStorage.getItem('token');
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
